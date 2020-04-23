41fba82415da56c93a63003904be90d5ffda086f
Radosław <radoslaw.madecki@gmail.com>
2020-04-23 14:42:08 +0200

    Added possibility to change displayed shows and added validation

--- src/app.js
+++ src/app.js
@@ -41,6 +41,8 @@ class TvMaze {
   }
 
   renderCards = shows => {
+    this.viewElems.showsWrapper.innerHTML = "";
+
     for (const { show } of shows) {
       this.createShowCard(show)
     }
@@ -48,11 +50,22 @@ class TvMaze {
 
   createShowCard = show => {
     const divCard = createDOMElem('div', 'card')
-    const img = createDOMElem('img', 'card-img-top', null, show.image.medium)
     const divCardBody = createDOMElem('div', 'card-body')
     const h5 = createDOMElem('h5', 'card-title', show.name)
-    const p = createDOMElem('p', 'card-text', show.summary)
     const btn = createDOMElem('button', 'btn btn-primary', 'Show details')
+    let img, p;
+
+    if (show.image) {
+      img = createDOMElem('img', 'card-img-top', null, show.image.medium)
+    } else {
+      img = createDOMElem('img', 'card-img-top', null, 'https://via.placeholder.com/210x295')
+    }
+
+    if (show.summary) {
+      p = createDOMElem('p', 'card-text', `${show.summary.slice(0, 80)}...`)
+    } else {
+      p = createDOMElem('p', 'card-text', 'There is no summary for that show yet.')
+    }
     
     divCard.appendChild(divCardBody)
     divCardBody.appendChild(img)
