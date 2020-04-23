f484d3cce4aed82e89a49fcade3380ccc1ffeae8
Radosław <radoslaw.madecki@gmail.com>
2020-04-23 12:20:27 +0200

    Set up requests

---
+++ src/requests.js
@@ -0,0 +1,7 @@
+export const getShowsByKey = key => {
+  return fetch(`http://api.tvmaze.com/search/shows?q=${key}`).then(resp => resp.json())
+}
+
+export const getShowById = id => {
+  return fetch(`http://api.tvmaze.com/shows/${id}?embed=cast`).then(resp => resp.json())
+}
